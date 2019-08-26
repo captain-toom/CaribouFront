b9636d0a9304ff82b6cf5d803308bccb4badc7eb
captain-toom <cami.thomas.work@gmail.com>
2019-08-26 10:37:06 +0200

    commit auth second part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HomebarComponent } from './homebar/homebar.component';
+import { HomebandComponent } from './homeband/homeband.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    HomebarComponent,
+    HomebandComponent
   ],
   imports: [
     FormsModule,
