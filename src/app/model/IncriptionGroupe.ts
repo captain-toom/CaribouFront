613f03165042b7c302cd11c15162652350e6e551
OneOfThemm <tanguy.chanzy@consultants-solutec.fr>
2019-08-27 14:15:11 +0200

    zef

--- src/app/model/IncriptionGroupe.ts
+++ src/app/model/IncriptionGroupe.ts
@@ -1,4 +1,20 @@
-export class BattleGroupe{
-    event: BattleGroupe =new BattleGroupe;
+import { BattleGroupe } from './BattleGroupe';
+import { Groupe } from './Groupe';
+
+export class InscriptionGroupe{
+    event: BattleGroupe =new BattleGroupe();
+    groupe: Groupe = new Groupe();
+
+    constructor(){}
+
+    
+    public set setEvent(event : BattleGroupe) {
+        this.event = event;
+    }
+
+    public set setGroupe(groupe : Groupe) {
+        this.groupe = groupe;
+    }
+    
     
 }
--- src/app/subscribe-event-groupe/subscribe-event-groupe.component.ts
+++ src/app/subscribe-event-groupe/subscribe-event-groupe.component.ts
@@ -4,6 +4,7 @@ import { Recherche } from '../model/Recherche';
 import { filter } from 'minimatch';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Router } from '@angular/router';
+import { InscriptionGroupe } from '../model/IncriptionGroupe';
 
 @Component({
   selector: 'app-subscribe-event-groupe',
@@ -18,7 +19,7 @@ export class SubscribeEventGroupeComponent implements OnInit {
   visibleevent = false;
   genres;
   genreschecked = [];
-  inscrig;
+  inscrig: InscriptionGroupe=new InscriptionGroupe();
   recherche: Recherche = new Recherche();
   constructor(private http:HttpClient, private routeur: Router) { }
 
@@ -41,6 +42,8 @@ search(){
 }
 
 inscrire(e){
+  this.inscrig.event=e;
+  this.inscrig.groupe.id=1;
   this.http.post('http://localhost:8083/groupeinscri', this.inscrig)
      .subscribe(data =>{
 
