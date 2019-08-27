1a0a6a0ba7a48d54c59f8865b15c83923dc9acb4
Hackaribou <arthurv59@gmail.com>
2019-08-27 14:09:35 +0200

    AJOUT DUN LOCAL STORAGE QUI ENVOIE LA SESSION ACTIVE

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { stringify } from '@angular/compiler/src/util';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable()
 export class AuthService {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient,
   ) { }
   typeconnect: String;
+  data;
   login(loginForm: any) {
     console.log('Tentative de connexion');
 
@@ -24,17 +28,38 @@ export class AuthService {
     if ((this.typeconnect == "client") == true) {
       this.setUser({ login: loginForm.username, roles: "CLIENT" });
       const redirectUrl = this.route.snapshot.queryParams['redirectUrl'] || '/home';
+      this.http.get('http://localhost:8083/client/mail/'+this.getUser().login)
+      .subscribe(
+        response => {
+          this.data = response;
+          this.setSession({id: this.data.id, mail : this.data.mail, photo : this.data.photo })
+          }
+      );
       this.router.navigate([redirectUrl]);
-      console.log("passage client")
+   
 
     } else if (this.typeconnect == "bar") {
       this.setUser({ login: loginForm.username, roles: "BAR" });
       const redirectUrl = this.route.snapshot.queryParams['redirectUrl'] || '/home_bar';
+      this.http.get('http://localhost:8083/bar/mail/'+this.getUser().login)
+      .subscribe(
+        response => {
+          this.data = response;
+          this.setSession({id: this.data.id, mail : this.data.mail, photo : this.data.photo, nom : this.data.nom, nomGerant : this.data.nomGerant, tel : this.data.tel, capacitemax : this.data.capacitemax })
+          }
+      );
       this.router.navigate([redirectUrl]);
 
     } else if (this.typeconnect == "groupe") {
       this.setUser({ login: loginForm.username, roles: "GROUPE" });
       const redirectUrl = this.route.snapshot.queryParams['redirectUrl'] || '/home_band';
+      this.http.get('http://localhost:8083/groupe/mail/'+this.getUser().login)
+      .subscribe(
+        response => {
+          this.data = response;
+          this.setSession({id: this.data.id, mail : this.data.mail, photo : this.data.photo, nom : this.data.nom, description : this.data.description, cachet : this.data.cachet, son : this.data.son, video: this.data.video })
+          }
+      );
       this.router.navigate([redirectUrl]);
 
     } else {
@@ -54,6 +79,7 @@ export class AuthService {
     console.log('Tentative de déconnexion');
     this.clearUser();
     this.clearTypeUser();
+    this.clearSession();
     this.router.navigate(['/login']);
   }
 
@@ -63,6 +89,14 @@ export class AuthService {
 
   setUser(user: any) {
     localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getSession() {
+    return JSON.parse(localStorage.getItem('session'));
+  }
+
+  setSession(session: any) {
+    localStorage.setItem('session', JSON.stringify(session));
   }
 
   getTypeUser() {
@@ -78,6 +112,10 @@ export class AuthService {
   }
   clearTypeUser() {
     localStorage.removeItem('type');
+  }
+
+  clearSession() {
+    localStorage.removeItem('session');
   }
 
   // METHODE DE VERIFICATION DES DROITS => cette fonctione retourne true ou fale => si un client veut se connecter sur page bar, renvoie false
