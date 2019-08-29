263d21968c47d259ea9f6b519b65c5df3c21a6d7
Hackaribou <arthurv59@gmail.com>
2019-08-29 18:02:20 +0200

    Page client améliorée

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,46 +7,47 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [AuthService]  
+  providers: [AuthService]
 })
 export class HomeComponent implements OnInit {
   FuturEvent;
   constructor(
     private router: Router,
     private authService: AuthService,
-    private http : HttpClient,
+    private http: HttpClient,
   ) { }
-
-  inscrire(){
+  nbEvents;
+  inscrire() {
     console.log("BJR");
   }
 
   ngOnInit() {
     document.body.classList.remove('bg-img');
-    this.http.get('http://localhost:8083/battlegroupes/client')    
-    .subscribe(
+    this.http.get('http://localhost:8083/battlegroupes/client')
+      .subscribe(
         response => {
+          this.nbEvents = Object.keys(response).length;
           console.log(response);
           this.FuturEvent = response;
         }
-    );
+      );
   }
 
   getLogin() {
     return this.authService.getUser().login;
     //return JSON.parse(localStorage.getItem('user')).login;
   }
-  
+
   logout() {
     console.log('Tentative de déconnexion');
-    return this.authService.logout();  
+    return this.authService.logout();
     //localStorage.removeItem('user');
-   // localStorage.removeItem('type');
+    // localStorage.removeItem('type');
     //this.router.navigate(['/login']);
   }
 
   hasAnyRole(roles: string[]) {
-   
+
 
     return this.authService.hasAnyRole(roles);
   }
